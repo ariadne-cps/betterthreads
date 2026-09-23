1874e96be9f381ab2c8cfc98315659c8aebec7ac
Luca Geretti <luca.geretti@icloud.com>
2026-09-23 21:29:26 +0200

    Cover remaining workload branches

--- test/test_workload.cpp
+++ test/test_workload.cpp
@@ -237,6 +237,24 @@ class TestWorkload {
 
 
 
+
+    void test_concurrent_logger_level_increase() {
+        if (ThreadManager::instance().maximum_concurrency() == 0) return;
+        ThreadManager::instance().set_concurrency(1);
+
+        Logger::instance().configuration().set_verbosity(0);
+        Logger::instance().increase_level(1);
+
+        auto result = std::make_shared<std::atomic<int>>(0);
+        StaticWorkloadType workload(&sum_all,result);
+        workload.append(1);
+
+        Logger::instance().decrease_level(1);
+        workload.process();
+
+        HELPER_TEST_EQUALS(result->load(),1)
+    }
+
     void test_concurrent_process_is_rejected() {
         ThreadManager::instance().set_concurrency(0);
         auto state = std::make_shared<ProcessConcurrencyState>();
@@ -352,6 +370,7 @@ class TestWorkload {
         HELPER_TEST_CALL(test_throw_serial_exception_later())
         HELPER_TEST_CALL(test_throw_concurrent_exception_immediately())
         HELPER_TEST_CALL(test_throw_concurrent_exception_later())
+        HELPER_TEST_CALL(test_concurrent_logger_level_increase())
         HELPER_TEST_CALL(test_concurrent_process_is_rejected())
         HELPER_TEST_CALL(test_progress_acknowledgement_is_serialised())
         HELPER_TEST_CALL(test_concurrent_exception_waits_for_running_tasks())
