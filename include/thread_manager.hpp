6b678ff114379794212eb8387575eb94c0d59fa6
Luca Geretti <luca.geretti@icloud.com>
2026-09-24 22:00:05 +0200

    Trace pool state across logger registry attachment

--- include/thread_manager.hpp
+++ include/thread_manager.hpp
@@ -62,9 +62,11 @@ class ThreadManager : public ThreadRegistryInterface {
         std::cerr << "ThreadManager::instance before attach: this=" << &instance
                   << " sizeof=" << sizeof(ThreadManager) << std::endl;
         if (not logger.has_thread_registry_attached()) {
+            std::cerr << "ThreadManager::instance pool before attach=" << instance._pool.num_threads() << std::endl;
             std::cerr << "ThreadManager::instance attaching registry" << std::endl;
             logger.attach_thread_registry(&instance);
             std::cerr << "ThreadManager::instance attached registry" << std::endl;
+            std::cerr << "ThreadManager::instance pool after attach=" << instance._pool.num_threads() << std::endl;
         }
         std::cerr << "ThreadManager::instance return: this=" << &instance << std::endl;
         return instance;
